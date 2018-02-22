a8a358fb433065a830f1055b409bc6628cb85b07
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-22 12:32:14 +1100

    CUST, Liam: 2018-02-22 12:32:14

---
+++ assessments/cat1_basicApp/liam.py
@@ -0,0 +1,16 @@
+name = input ('What is your name? ' )
+print ('hello' + ' ' + name + '! my name is Liam')
+if name == ('Erik'):
+    print('Hey sir, im learning') 
+    print ('slowly')
+if name == ('Mr koopmans') :
+    print ('Hey sir, im learning')
+if name == ('ethan') :
+    print ('Get off minecaft and do some work')
+answer = input ('did you like my code? ')
+if answer == ('yes'):
+    print (' That is so good') 
+if answer == ('no') :
+    print ('Ur lame')
+if answer == ('Yes') :
+    print ('YAY') 
