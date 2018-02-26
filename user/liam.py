a7491d857b17589633a5bef6d375438929cdcad3
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-27 10:54:39 +1100

    CUST, Liam: 2018-02-27 10:54:39

--- user/liam.py
+++ user/liam.py
@@ -1,5 +1,22 @@
+# question
 name = input ('What is your name? ' )
-print (name)
 print ('hello' + ' ' + name + '! my name is Liam')
-if name (Erik):
+# if answers
+if name == ('Erik'):
     print('Hey sir, im learning') 
+    print ('slowly')
+if name == ('Mr koopmans') :
+    print ('Hey sir, im learning')
+if name == ('ethan') :
+    print ('Get off minecaft and do some work')
+if name == ('jet') :
+    print ('Nice frosted tips')
+# new question
+answer = input ('did you like my code? ')
+# if answers
+if answer == ('yes'):
+    print (' That is so good') 
+if answer == ('no') :
+    print ('Ur lame')
+if answer == ('Yes') :
+    print ('YAY') 
