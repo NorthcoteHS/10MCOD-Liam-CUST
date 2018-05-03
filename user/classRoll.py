fab861453395b3c8eb39cd11c2f37c15cec52231
Liam Cust <liamcust5@gmail.com>
2018-05-03 11:52:47 +1000

    adding programs

---
+++ user/classRoll.py
@@ -0,0 +1,10 @@
+roll = ['Jessica', 'Emily', 'Jordan', 'Kayley', 'Bruce', 'Micheal', 'Everett', 'Lisa', 'Sam', 'Noah']
+
+print (roll[2])
+enrolment = len(roll)
+roll.append('James')
+del roll[2]
+roll[4] = 'Mike'
+print(roll)
+lastItem = roll[-7]
+print(lastItem)
